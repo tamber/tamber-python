2624840333d7df662aa5b75ffef5f1799ef44f9b
Alexander Robbins <alexi@tamber.com>
2016-02-06 23:43:36 -0800

    example.py updated

--- example.py
+++ example.py
@@ -1,7 +1,25 @@
 import tamber
 
 tamber.api_key = '80r2oX10Uw4XfZSxfh4O'
-item = tamber.Item(id='68753A444D6F', properties={'height': 23.4}, tags=['amazing', 'rustic'], created=1446417346)
-item.create()
 
-print(item.__dict__)
+# Track an Event
+try:
+	event = tamber.Event.track(
+		user='user_rlox8k927z7p',
+		behavior='like',
+		item='item_wmt4fn6o4zlk'
+	)
+	print event
+except tamber.TamberError as err:
+	print err
+
+# Get Recommendations
+try:
+	recs = tamber.Discover.recommended(
+		user='user_rlox8k927z7p',
+	)
+	for rec in recs:
+    	print "item:%s  score%s\n" % (rec['item'], rec['score'])
+    	
+except tamber.TamberError as err:
+	print err
